0a861bb3056ca4e913e71a05d41644e0ef9f14b2
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-03-28 12:13:02 +0100

    build

--- src/pages/contact-page/contact-page.jsx
+++ src/pages/contact-page/contact-page.jsx
@@ -17,9 +17,43 @@ export default class ContactPage extends React.Component {
     }
 	
 	validateContactTerm(contactInput){
-		//let app = this;
-		console.log(contactInput);
+		let app = this;
 		
+		//validate first name input
+		if (contactInput.fName === "" || contactInput.fName === undefined){
+			app.setState({isFirstName: "Please enter a valid first name"});
+			return;
+		}else{
+			app.setState({isFirstName: false});
+		}
+		
+		//validate lastname input
+		if (contactInput.lName === "" || contactInput.lName === undefined){
+			app.setState({isLastName: "Please enter a valid last name"});
+			return;
+		}else{
+			app.setState({isLastName: false});
+		}
+		
+		//validate email input
+		let regExEmail = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,4}$/;
+
+		if (!regExEmail.test(contactInput.email)){
+			app.setState({isEmail: "Please enter a valid e-mail adress"});
+			return;
+		}else{
+			app.setState({isEmail: false});
+		}
+		
+		//validate phone input
+		let regExPhone = /^(\d{3}\.\d{3}\.\d{4})|(\d{3}-\d{3}-\d{4})|(\d{3}\s\d{3}\s\d{4})$/;
+	
+		if (!regExPhone.test(contactInput.phone)){
+			app.setState({isPhone: "Please enter a valid phone number (ex: 123 123 1234 or 123.123.1234 or 123-123-1234)"});
+			return;
+		}else{
+			app.setState({isPhone: false});
+		}
 	}
 	
 	componentDidMount() {
