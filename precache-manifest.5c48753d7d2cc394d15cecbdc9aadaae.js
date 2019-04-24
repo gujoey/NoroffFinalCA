ea8c555b5a8293820d512ba87540059f7dfae84c
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-04-24 12:16:23 +0200

    Updates

---
+++ precache-manifest.5c48753d7d2cc394d15cecbdc9aadaae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5407c472ae32888bf9f",
+    "url": "/card-game-app/static/js/runtime~main.ad4d0bbc.js"
+  },
+  {
+    "revision": "a1998cf05698bc240811",
+    "url": "/card-game-app/static/js/main.fc719a38.chunk.js"
+  },
+  {
+    "revision": "c808d54ca7ce7b0485c7",
+    "url": "/card-game-app/static/js/2.b9124f10.chunk.js"
+  },
+  {
+    "revision": "a1998cf05698bc240811",
+    "url": "/card-game-app/static/css/main.5e5f46d8.chunk.css"
+  },
+  {
+    "revision": "516c5ad2b185d26c9e68b20e83233208",
+    "url": "/card-game-app/index.html"
+  }
+];
